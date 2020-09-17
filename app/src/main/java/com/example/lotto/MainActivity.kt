e9ea2ad2c6f6031a9a465a4265755b63a927ca44
SSong-develop <thdgnsrl123@naver.com>
2020-09-18 01:41:50 +0900

    init

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lotto_number = arrayListOf(number1,number2,number3,number4,number5,number6)
+
+        val countDownTimer = object : CountDownTimer(3000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                lotto_number.forEach{
+                    val randomNumber = (Math.random() * 45 + 1).toInt()
+                    it.text = "${randomNumber}"
+                }
+            }
+
+            override fun onFinish() {
+
+            }
+        }
+        lottie_wheel.setOnClickListener {
+            if(lottie_wheel.isAnimating){
+                lottie_wheel.cancelAnimation()
+                countDownTimer.cancel()
+            }else{
+                lottie_wheel.playAnimation()
+                countDownTimer.start()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/lotto/Splash.kt
+++ app/src/main/java/com/example/lotto/Splash.kt
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import androidx.core.os.postDelayed
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.activity_splash.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class Splash : AppCompatActivity() {
 
-    private val time = 2000
+    private val time : Long = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(time)
             startActivity(Intent(this@Splash,MainActivity::class.java))
             finish()
-        }, time.toLong())
+        }
+
+        /*Handler(Looper.getMainLooper).postDelayed({ // Runnble 객체와 time을 파라미터로 받는다
+            startActivity(Intent(this@Splash,MainActivity::class.java))
+            finish()
+        }, time)*/
     }
-
-
 }
